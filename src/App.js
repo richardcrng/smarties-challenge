5918c38c93a97db632d903f95ea0dbb192fe7687
Richard Ng <richardcrng@gmail.com>
2019-07-03 15:17:57 +0100

    use state in App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Smarties from './components/molecules/Smarties';
 import SmartiesGroup from './components/organisms/SmartiesGroup/SmartiesGroup';
 
 function App() {
+  const [state, setState] = React.useState({
+    orange: 2,
+    yellow: 10,
+    pink: 5,
+    purple: 2,
+    red: 2
+  })
+
   return (
     <div>
       <SmartiesGroup>
-        <Smarties n={2} color="orange" />
-        <Smarties n={10} color="yellow" />
-        <Smarties n={5} color="pink" />
-        <Smarties n={2} color="purple" />
-        <Smarties n={2} color="red" />
+        {Object.entries(state).map(([color, n]) => (
+          <Smarties key={color} {...{ color, n }} />
+        ))}
       </SmartiesGroup>
     </div>
   );
